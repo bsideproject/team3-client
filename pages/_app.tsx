d5debecb3438636234984ace6b6c4c3093f47e94
limeunseop <dmstjq12@naver.com>
2022-07-04 23:13:50 +0900

    Add: store Provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
+import Store from '@/models/store'
 import '@/styles/global.css'
+import { Provider } from 'mobx-react'
 import type { AppProps } from 'next/app'
 
+const store = new Store()
+
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Provider {...store.getStores()}>
+      <Component {...pageProps} />
+    </Provider>
+  )
 }
