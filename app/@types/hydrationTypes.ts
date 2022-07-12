f5b09b6dc33f056792a76576b553e21fa3533bca
limeunseop <dmstjq12@naver.com>
2022-07-12 19:28:55 +0900

    Add; WithHydration Type

--- app/@types/hydrationTypes.ts
+++ app/@types/hydrationTypes.ts
@@ -1,5 +1,9 @@
 import { Todo } from './todoTypes'
 
+export type WithHydration = {
+  hydrationData: StoreHydration
+}
+
 export type StoreHydration = {
   todosModel?: TodosModelHydration
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,22 @@
 import TodoList from '@/providers/TodoList'
 import { GetServerSideProps } from 'next'
 import { todosFixture } from '@/models/domain/__fixtures__/todosFixture'
-import { StoreHydration } from '@/types/hydrationTypes'
+import { WithHydration } from '@/types/hydrationTypes'
 
 export default function Home() {
   return <TodoList />
 }
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
+export const getServerSideProps: GetServerSideProps<WithHydration> = async (
+  context
+) => {
   return {
     props: {
       hydrationData: {
         todosModel: {
           todos: todosFixture,
         },
-      } as StoreHydration,
+      },
     },
   }
 }
