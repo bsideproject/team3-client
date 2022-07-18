f2120eb93cecccf7bc675a48e4c8bbdd20c86421
limeunseop <dmstjq12@naver.com>
2022-07-18 16:38:40 +0900

    Add refreshToken API Proxy

--- pages/api/[...path].js
+++ pages/api/[...path].js
@@ -19,7 +19,8 @@ export default function handler(req, res) {
     let requestUrl = req.url ?? 'UNDEFINED_URL'
 
     const pathname = url.parse(requestUrl).pathname
-    const isLogin = pathname === '/api/login'
+    const isLogin = pathname === '/api/auth/login'
+    const isRefreshToken = pathname === '/api/auth/refreshToken'
 
     const cookies = new Cookies(req, res)
     const authToken = cookies.get('auth-token')
@@ -34,6 +35,10 @@ export default function handler(req, res) {
 
     if (isLogin) {
       proxy.once('proxyRes', interceptLoginResponse)
+    }
+
+    if (isRefreshToken) {
+      proxy.once('proxyRes', interceptRefreshToken)
     }
 
     proxy.once('error', reject)
@@ -52,15 +57,49 @@ export default function handler(req, res) {
 
       proxyRes.on('end', () => {
         try {
-          const { authToken } = JSON.parse(apiResponseBody)
+          const { authToken, refreshToken } = JSON.parse(apiResponseBody)
 
           const cookies = new Cookies(req, res)
           cookies.set('auth-token', authToken, {
             httpOnly: true,
             sameSite: 'lax',
           })
+          cookies.set('refresh-token', refreshToken, {
+            httpOnly: true,
+            sameSite: 'lax',
+          })
 
+          // Redirect로 바꿔야할듯
           res.status(200).json({ loggedIn: true })
+          resolve()
+        } catch (err) {
+          reject(err)
+        }
+      })
+    }
+
+    function interceptRefreshToken(proxyRes, req, res) {
+      let apiResponseBody = ''
+      proxyRes.on('data', (chunk) => {
+        apiResponseBody += chunk
+      })
+
+      proxyRes.on('end', () => {
+        try {
+          const { authToken } = JSON.parse(apiResponseBody)
+
+          if (authToken) {
+            const cookies = new Cookies(req, res)
+            cookies.set('auth-token', authToken, {
+              httpOnly: true,
+              sameSite: 'lax',
+            })
+
+            res.redirect('/')
+          } else {
+            res.redirect('/login')
+          }
+
           resolve()
         } catch (err) {
           reject(err)
