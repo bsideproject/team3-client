cf6314efa0e24c3d963b42c5df7355b912bab79b
limeunseop <dmstjq12@naver.com>
2022-07-28 20:53:34 +0900

    Update Global CSS Setting

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import StoreProvider from '@/models/StoreProvider'
-import '@/styles/global.css'
+import '@/styles/index.scss'
 import type { AppProps } from 'next/app'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
