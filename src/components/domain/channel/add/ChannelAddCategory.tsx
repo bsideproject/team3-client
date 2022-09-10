b9569a1cd371dfee520af69b7c57ab05996ff16c
limeunseop <dmstjq12@naver.com>
2022-09-11 00:31:20 +0900

    채널등록 카테고리 input value 연결

--- src/components/domain/channel/add/ChannelAddCategory.tsx
+++ src/components/domain/channel/add/ChannelAddCategory.tsx
@@ -70,7 +70,7 @@ const ChannelAddCategory = ({
               placeholder="채널 카테고리를 지정해주세요"
               options={categoryOptions}
               isLoading={isLoading}
-              value={categoryOptions.find(
+              value={categoryOptions?.find(
                 (option) => option.value === selectedCategory
               )}
               onChange={handleSelectCategory}
